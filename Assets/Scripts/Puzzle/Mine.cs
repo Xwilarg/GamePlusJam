42326b4362328f1c14fa16dd6fb3a8db4372ce80
Xwilarg <xwilarg@yahoo.fr>
2022-03-12 18:56:49 +0100

    Add mines enabling mechanism

---
+++ Assets/Scripts/Puzzle/Mine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GamesPlusJam.Puzzle
+{
+    public class Mine : MonoBehaviour
+    {
+        public void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}
