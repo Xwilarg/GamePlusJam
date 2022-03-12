e08d5b5df0dfa7db1fe0f655b55069f14d02d322
Xwilarg <xwilarg@yahoo.fr>
2022-03-12 15:20:04 +0100

    Add line puzzle

---
+++ Assets/Scripts/Puzzle/UpDown.cs
@@ -0,0 +1,44 @@
+﻿using GamesPlusJam.Action.Interaction;
+using UnityEngine;
+
+namespace GamesPlusJam.Puzzle
+{
+    public class UpDown : AInteraction
+    {
+        public override bool IsOneWay => true;
+
+        public override void InteractOff(PlayerController pc)
+        { }
+
+        public override void InteractOn(PlayerController pc)
+        {
+            Manager.Activate(Index);
+        }
+
+        [SerializeField]
+        private float _speed;
+
+        private float _prog = 0f;
+        private bool _goUp = false;
+        private Vector3 _orPos;
+
+        public int Index { set; private get; }
+        public UpDownManager Manager { set; private get; }
+
+        private void Update()
+        {
+            _prog = Mathf.Clamp01(_prog + Time.deltaTime * _speed * (_goUp ? 1f : -1f));
+            transform.position = Vector3.Lerp(_orPos, _orPos + Vector3.down, _prog);
+        }
+
+        private void Start()
+        {
+            _orPos = transform.position;
+        }
+
+        public void Activate()
+        {
+            _goUp = !_goUp;
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzle/UpDownManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace GamesPlusJam.Puzzle
+{
+    public class UpDownManager : MonoBehaviour
+    {
+        [SerializeField]
+        public UpDown[] _upDowns;
+
+        private void Start()
+        {
+            for (int i = 0; i < _upDowns.Length; i++)
+            {
+                _upDowns[i].Index = i;
+                _upDowns[i].Manager = this;
+            }
+        }
+
+        public void Activate(int index)
+        {
+            _upDowns[index].Activate();
+            if (index > 0)
+            {
+                _upDowns[index - 1].Activate();
+            }
+            if (index < _upDowns.Length - 1)
+            {
+                _upDowns[index + 1].Activate();
+            }
+        }
+    }
+}
