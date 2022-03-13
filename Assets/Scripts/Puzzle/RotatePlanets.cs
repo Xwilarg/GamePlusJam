d7544ada5a994a4b6e55653479622325636c4349
Xwilarg <xwilarg@yahoo.fr>
2022-03-13 18:33:31 +0100

    Add planets rotation

--- Assets/Scripts/Puzzle/RotatePlanets.cs
+++ Assets/Scripts/Puzzle/RotatePlanets.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace GamesPlusJam.Puzzle
 {
     public class RotatePlanets : MonoBehaviour
     {
+        private void Start()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var c = transform.GetChild(i);
+                c.transform.rotation = Quaternion.Euler(
+                    c.transform.rotation.x,
+                    Random.Range(0, 360),
+                    c.transform.rotation.z
+                    );
+            }
+        }
+
+        private void Update()
+        {
+            transform.rotation = Quaternion.Euler(
+                x: transform.rotation.eulerAngles.x,
+                y: transform.rotation.eulerAngles.y + Time.deltaTime * -50f,
+                z: transform.rotation.eulerAngles.z
+                );
+        }
     }
 }
